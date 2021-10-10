d3db558ffdae5ee88dd82c13484a24d80112c5c5
Mario Sylainx S Gauthier <sylainx7@gmail.com>
2021-10-10 07:20:25 -0400

    Correction bug deconnexion 2

--- login/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-     try{
-         $bdd = new PDO('mysql:host=localhost;dbname=seconnecter;charset=utf8', 'root', 'root');
-     } catch(Exception $e)
-       {
-           die('Erreur'.$e->getmessage());
-       }
--- login/deconnexion.php
+++ login/deconnexion.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     session_destroy();
-    header('Location: ../dashboard.php');
+    header('Location: ../index.php');
 
 
